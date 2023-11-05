758424726319cc31f6059ef112089fc343199481
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2023-11-05 12:07:48 +0530

    Roboshop with terraform setup

--- state.tf
+++ state.tf
@@ -1,5 +1,7 @@
-  backend "s3" {
-    bucket = "parameters-statefile"
-    key    = "params/terraform.tfstate"
-    region = "us-east-1"
+  terraform {
+    backend "s3" {
+      bucket = "parameters-statefile"
+      key    = "params/terraform.tfstate"
+      region = "us-east-1"
+    }
   }
